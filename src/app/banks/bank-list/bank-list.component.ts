9b40e08c624c0c2694d3159a381487676e694b7f
Mifta Uddin <muddin94@outlook.com>
2018-08-13 02:26:16 -0400

    Added bank-list.component

--- src/app/banks/bank-list/bank-list.component.ts
+++ src/app/banks/bank-list/bank-list.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-bank-list',
-  templateUrl: './bank-list.component.html'
+  templateUrl: './bank-list.component.html',
+  styleUrls: ['./bank-list.component.css']
 })
-export class BankListComponent{}
+export class BankListComponent{
+  banks = [
+    {name: 'Capital One', description: 'This is Capital One\'s description'},
+    {name: 'Wells Fargo', description: 'This is Wells Fargo\'s description'},
+    {name: 'Chase', description: 'This is Chase\'s description'}
+  ]
+}
