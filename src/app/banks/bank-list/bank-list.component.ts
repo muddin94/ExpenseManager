53e4d68fffee5b451710683bbced49f0645519f3
Mifta Uddin <muddin94@outlook.com>
2018-08-13 02:26:16 -0400

    Added bank-list.component

--- src/app/banks/bank-list/bank-list.component.ts
+++ src/app/banks/bank-list/bank-list.component.ts
@@ -7,8 +7,15 @@ import { Component } from '@angular/core';
 })
 export class BankListComponent{
   banks = [
+<<<<<<< HEAD
     {name: 'Capital One', description: 'This is Capital One\'s description'},
     {name: 'Wells Fargo', description: 'This is Wells Fargo\'s description'},
     {name: 'Chase', description: 'This is Chase\'s description'}
   ]
+=======
+    {name: 'Capital One', content: 'What\'s in your wallet?'},
+    {name: 'Wells Fargo', content: 'Together we\'ll go far!'},
+    {name: 'Chase', content: 'The right relationship is everything.'}
+  ];
+>>>>>>> f64a9d6eba7b3372f037f4d4526843f431aa1ef3
 }
