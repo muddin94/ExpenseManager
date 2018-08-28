9692517b9b602b4ad650f96f07ad97bb86eb00e5
mifta.uddin <mifta.uddin@accenture.com>
2018-08-28 14:04:01 -0400

    added mongodb Atlas connectivity using mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Bank = require('./models/bank');
+
 const app = express();
+
+mongoose.connect('mongodb+srv://root:N3gr8F18PBadnkC7@cluster0-mo2we.mongodb.net/test?retryWrites=true')
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(() =>{
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -20,7 +31,10 @@ app.use((req, res, next) =>{
 });
 
 app.post('/api/banks', (req, res, next) =>{
-  const bank = req.body;
+  const bank = new Bank({
+    name: req.body.name,
+    value: req.body.value
+  });
   console.log(bank);
   res.status(201).json({
     message: 'Bank added successfully.'
