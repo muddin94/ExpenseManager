30eb805e7e3c86f544a0899c9ab3b1f99419dfc8
mifta.uddin <mifta.uddin@accenture.com>
2018-08-28 14:33:46 -0400

    enabled the bank objects to be mapped correctly

--- src/app/banks/banks.service.ts
+++ src/app/banks/banks.service.ts
@@ -2,6 +2,7 @@ import { Bank } from './bank.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class BanksService {
@@ -11,9 +12,18 @@ export class BanksService {
   constructor(private http: HttpClient) {}
 
   getBanks() {
-    this.http.get<{message: string, banks: Bank[]}>('http://localhost:3000/api/banks')
-      .subscribe((bankData) => {
-        this.banks = bankData.banks;
+    this.http.get<{message: string, banks: any }>('http://localhost:3000/api/banks')
+      .pipe(map((bankData) => {
+        return bankData.banks.map(bank => {
+          return {
+            name: bank.name,
+            value: bank.value,
+            id: bank._id
+          };
+        });
+      }))
+      .subscribe((transformedBanks) => {
+        this.banks = transformedBanks;
         this.banksUpdated.next([...this.banks]);
       });
   }
