3b957091649290f33232d4e72c238da6b6891011
mifta.uddin <mifta.uddin@accenture.com>
2018-08-28 14:03:11 -0400

    corrected variable name from postSchema to bankSchema

--- backend/models/bank.js
+++ backend/models/bank.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const postSchema = mongoose.Schema({
+const bankSchema = mongoose.Schema({
   name: { type: String, required: true },
   value: { type: String, required: true }
 });
